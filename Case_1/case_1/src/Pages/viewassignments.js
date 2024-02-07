07135b601a7687afad4de94c6d6ffd4534e5e751
Victor D <victordekker55@gmail.com>
2024-02-07 14:24:06 +0100

    add ed

---
+++ Case_1/case_1/src/Pages/viewassignments.js
@@ -0,0 +1,38 @@
+
+
+
+const ViewAssignments = () => {
+    const [assignments, setAssignments] = useState([]);
+    const [referees, setReferees] = useState([]);
+    const [selectedAssignment, setSelectedAssignment] = useState('');
+    const [selectedReferee, setSelectedReferee] = useState('');
+
+    const getAssignments = async () => {
+        try {
+            const response = await axios.get("http://localhost:5000/assignment/get", {
+                headers: {
+                    'Authorization': sessionStorage.getItem('accessToken')
+                }
+            });
+            console.log("Assignments", response.data);
+            setAssignments(response.data);
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+
+    return (
+        <div>
+            <h1>View Assignments</h1>
+            {assignments.map((assignment) => (
+                <div key={assignment.id}>
+                    <p>Match: {assignment.match_id}</p>
+                    <p>Referee: {assignment.referee_id}</p>
+                </div>
+            ))}¨
+
+        </div>
+    )
+}
+
+export default ViewAssignments;
